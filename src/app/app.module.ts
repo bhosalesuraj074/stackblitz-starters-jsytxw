cf9173797351ef2727ab4deef285419b1300abef
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-04 11:04:45 +0530

    1 file modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppCompoent } from './app.component';
-
+import { Route, RouterModule } from '@angular/router';
+import { LaptopComponent } from './product/laptop/laptop.component';
+const route: Route[] = [{ path: '', component: LaptopComponent }];
 @NgModule({
   declarations: [AppCompoent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(route)],
   bootstrap: [AppCompoent],
 })
 export class AppModule {}
